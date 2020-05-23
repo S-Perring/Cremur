6457b59ac0293095938cb3bbd2e217c5a182809c
S-Perring <steve_perring@yahoo.co.uk>
2020-05-23 20:15:38 +0000

    Remove requirments file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = 'cremur'
-app.config["MONGO_URI"] = 'mongodb+srv://Cremur:<Cremur>@cluster0-odwyr.mongodb.net/cremur?retryWrites=true&w=majority')
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
@@ -18,5 +18,5 @@ def get_tasks():
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=(os.environ.get('PORT')),
+            port=int(os.environ.get('PORT')),
             debug=True)
